80fe49a8d6fd93f8d9ccf7584d766c26b96e8d0d
Albert Avksentev <stry.bout.eyes@gmail.com>
2023-01-18 09:32:45 +0600

    Add microservices in workspaces

--- services/gateway-service/src/main.rs
+++ services/gateway-service/src/main.rs
@@ -5,18 +5,29 @@ use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 use serde::Deserialize;
 use std::{sync::Mutex, time::Duration};
 use tokio;
+use utoipa::{OpenApi, ToSchema};
 
 mod resources;
 
+#[derive(OpenApi)]
+#[openapi(paths(user), components(schemas(Info)))]
+struct ApiDoc;
+
+#[derive(ToSchema)]
 struct AppState {
     app_name: String,
     counter: Mutex<i32>,
 }
 
-#[derive(Deserialize)]
+#[derive(ToSchema, Deserialize)]
 struct Info {
-    user_id: u32,
+    user_id: String,
     friend: String,
+}
+
+#[derive(ToSchema, Deserialize)]
+struct QueryInfo {
+    nickname: Option<String>,
 }
 
 #[get("/")]
@@ -51,12 +62,69 @@ async fn sleep() -> impl Responder {
     HttpResponse::Ok().body("wake")
 }
 
+#[utoipa::path(
+    get,
+    path = "users/{user_id}/{friend}",
+    responses(
+        (status = 200, description = "user found succesfully", body = Info),
+        (status = 404, description = "user was not found")
+    ),
+    params(
+        ("id" = u64, Path, description = "user database id to get user for"),
+    )
+)]
 #[get("/{user_id}/{friend}")]
-async fn user(info: web::Path<Info>) -> Result<String> {
+async fn user(info: web::Path<Info>, query_info: web::Query<QueryInfo>) -> Result<String> {
     Ok(format!(
-        "Welcome {}, user_id {}!",
-        info.friend, info.user_id
+        "Welcome {}, user_id {}! Query nickname {:?}",
+        info.friend, info.user_id, query_info.nickname
     ))
+}
+
+#[get("/api")]
+async fn api() -> impl Responder {
+    let open_api = ApiDoc::openapi().to_yaml().unwrap();
+    HttpResponse::Ok().body(open_api)
+}
+
+#[get("/redoc")]
+async fn redoc() -> impl Responder {
+    let html = r#"
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Redoc</title>
+            <!-- needed for adaptive design -->
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link
+            href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700"
+            rel="stylesheet"
+            />
+
+            <!--
+            Redoc doesn't change outer page styles
+            -->
+            <style>
+            body {
+                margin: 0;
+                padding: 0;
+            }
+            </style>
+        </head>
+        <body>
+            <!--
+            Redoc element with link to your OpenAPI definition
+            -->
+            <redoc spec-url="http://localhost:8080/api"></redoc>
+            <!--
+            Link to Redoc JavaScript on CDN for rendering standalone element
+            -->
+            <script src="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"></script>
+        </body>
+        </html>
+    "#;
+    HttpResponse::Ok().content_type("text/html").body(html)
 }
 
 #[actix_web::main]
@@ -81,6 +149,8 @@ async fn main() -> std::io::Result<()> {
             .service(web::scope("/users").service(show_users).service(user))
             .service(hello)
             .service(echo)
+            .service(api)
+            .service(redoc)
             .service(
                 web::scope("/hey")
                     // guard that filter requests on 'Host' header field
