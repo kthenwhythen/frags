536895eaaecd21dd90f23aef3a2db576acf1e58f
Albert Avksentev <stry.bout.eyes@gmail.com>
2023-02-03 14:45:07 +0600

    :bulb: USER
    Add comment to useless code

--- services/user-service/src/constants.rs
+++ services/user-service/src/constants.rs
@@ -1,4 +1,4 @@
 pub const PORT: &str = "9002";
 pub const IP: &str = "0.0.0.0";
-pub const CONTEXT_PATH_V1: &str = "/user-service/v1";
+// pub const CONTEXT_PATH_V1: &str = "/user-service/v1";
 pub const SERVICE_NAME: &str = env!("CARGO_PKG_NAME");
